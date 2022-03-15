8db54f7347dba00137b2e946338989780229800a
justinmolnar <1justin.molnar@gmail.com>
2022-03-15 16:24:14 -0600

    added footer link, styling

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,3 +1,4 @@
+//marquee
 const root = document.documentElement;
 const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
 const marqueeContent = document.querySelector(".marquee-content");
